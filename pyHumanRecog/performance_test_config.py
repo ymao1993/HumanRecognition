4d63f5b28b1c12c6529133d920382852be6bac9b
Yu <yumao@cmu.edu>
2017-05-09 02:14:28 -0400

    :)

--- pyHumanRecog/performance_test_config.py
+++ pyHumanRecog/performance_test_config.py
@@ -5,8 +5,8 @@ random.seed(0)
 # feature configuration
 features = \
 {
-    'face': {'path': 'feat/face.feat', 'weight': 0.5, 'similarity_metric': 'euclidean'},
-    'body': {'path': 'feat/body.feat', 'weight': 1.0, 'similarity_metric': 'cosine'}
+    'face': {'path': 'feat/face.feat', 'weight': 0.5},
+    'body': {'path': 'feat/body.feat', 'weight': 1.0}
 }
 
 # K in KNN in prediction

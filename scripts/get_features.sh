ae0c18e75afbba7a6af806246ecfa37490ff956b
Yu Mao <yumao@cmu.edu>
2017-05-20 19:00:57 -0700

    Update get_features.sh

--- scripts/get_features.sh
+++ scripts/get_features.sh
@@ -1,3 +1,5 @@
 mkdir -p feat
-wget --no-check-certificate https://www.dropbox.com/s/b7bixitllm7cyly/body.feat?dl=0 -O feat/body.feat
-wget --no-check-certificate https://www.dropbox.com/s/hguj7c83rc1g1tt/face.feat?dl=0 -O feat/face.feat
+wget --no-check-certificate https://www.dropbox.com/s/br2imgqjrgl3w3v/body2.feat?dl=0 -O feat/body2.feat
+wget --no-check-certificate https://www.dropbox.com/s/z3lkdjdayiilx71/face.feat?dl=0 -O feat/face.feat
+wget --no-check-certificate https://www.dropbox.com/s/t45vulkyn6e3eqy/head.feat?dl=0 feat/head.feat
+wget --no-check-certificate https://www.dropbox.com/s/zm9oryqe66qpram/upperbody.feat?dl=0 feat/upperbody.feat
